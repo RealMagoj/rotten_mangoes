5ae48ae0fd4fd5525fbc019e648918faa8208b7e
RealMagoj <RealMagoj@users.noreply.github.com>
2016-03-02 11:48:25 -0500

    16. using some helper methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -19,6 +19,6 @@ class Review < ActiveRecord::Base
     numericality: { greater_than_or_equal_to: 1 }
 
   validates :rating_out_of_ten,
-    numericalityL { less_than_or_equal_to: 10 }
+    numericality: { less_than_or_equal_to: 10 }
 
 end
